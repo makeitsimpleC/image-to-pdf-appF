0ae008af78495b5b4a059a566d461cd5d97dc27f
makeitsimpleC <convert@makeit-simple.de>
2025-04-11 04:34:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,9 +27,6 @@ def convert():
     except Exception as e:
         return {'error': str(e)}, 500
 
-import os
-
 if __name__ == '__main__':
-    import logging
-    logging.basicConfig(level=logging.INFO)
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), debug=False)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
